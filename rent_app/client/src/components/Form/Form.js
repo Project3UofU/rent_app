42ae8f45391942efdd93c0755a331026f1c8c11d
MCPolichette <mark@polichette.com>
2018-09-19 16:46:14 -0600

    Bootstrap added, and more folders made

---
+++ rent_app/client/src/components/Form/Form.js
@@ -0,0 +1,5 @@
+import React from "react";
+import Input from "./Input";
+
+
+export default Form;
---
+++ rent_app/client/src/components/Form/Input.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+// Input Design, to simplify the layout.
+
+export default input;
