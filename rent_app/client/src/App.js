7f0e3859aff0e74a709077bb204a392c2b64d68e
MCPolichette <mark@polichette.com>
2018-09-19 19:42:07 -0600

    successful routing

--- rent_app/client/src/App.js
+++ rent_app/client/src/App.js
@@ -14,6 +14,7 @@ import './App.css';
         <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/Landlord" component={Landlord} />
           <Route component={NoMatch} />
         </Switch>
       </div>
