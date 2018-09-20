f8d0b8add7bd7cbdcd22b317bff3b4929359384f
MCPolichette <mark@polichette.com>
2018-09-20 14:18:09 -0600

    Rebuilding Form formats.

--- rent_app/client/src/App.js
+++ rent_app/client/src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-
+// Import Pages
 import Home from "./pages/Home";
 import Landlord from "./pages/Landlord";
 import NoMatch from "./pages/NoMatch";
+// import Landing from "./pages/Landing";
+
+// Import Components
 import Nav from "./components/Nav";
+
+// Global CSS (unedited form Create-React-App command!)
 import './App.css';
   
   const App = () => (
