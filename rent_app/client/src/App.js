a4b7621643f7bbdd48dc2d15c338dd18d4e9640d
William A. Swensen <36058969+Ponker25@users.noreply.github.com>
2018-09-25 20:12:07 -0600

    Update App.js

--- rent_app/client/src/App.js
+++ rent_app/client/src/App.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 // Import Pages
 // import Home from "./pages/Home";
-// import Landlord from "./pages/Landlord";
+import Landlord from "./pages/Landlord";
 import Landing from "./pages/Landing";
 import Register from "./pages/Register";
 import NoMatch from "./pages/NoMatch";
@@ -87,7 +87,7 @@ class App extends Component {
         <Switch>
           <Route exact path="/" render={() =>       <Home user={this.state.user} />
           }/>
-          {/*<Route exact path="/Landlord" component={Landlord} />*/}
+          <Route exact path="/Landlord" component={Landlord} />
           <Route exact path="/landing" component={Landing} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/test" component={UnitPage} />
