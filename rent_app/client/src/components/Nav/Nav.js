b5f3915cb2f6bd6106af7896dd8f4c05abc0436c
MCPolichette <mark@polichette.com>
2018-09-29 21:01:28 -0600

    Home Page Basics built

--- rent_app/client/src/components/Nav/Nav.js
+++ rent_app/client/src/components/Nav/Nav.js
@@ -1,48 +1,49 @@
 import React from "react";
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import "./Nav.css"
 
 const Nav = (props) => {
-    if (props.loggedIn) {
-      return (
-        <nav className="navbar navbar-invert">
-          <ul className="nav">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                Home
+  if (props.loggedIn) {
+    return (
+      <nav className="navbar navbar-invert">
+
+        <ul className="nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              <img src="/images/RentKeeper-01.png" class="img-fluid" alt="RentKeeper" />
+            </Link>
+          </li>
+          <li>
+            <Link to="#" className="nav-link" onClick={props._logout}>
+              Logout
               </Link>
-            </li>
-            <li>
-              <Link to="#" className="nav-link" onClick={props._logout}>
-                Logout
+          </li>
+        </ul>
+      </nav>
+    )
+  } else {
+    return (
+      <nav className="navbar">
+        <ul className="nav">
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              <img src="/images/RentKeeper-01.png" alt="RentKeeper" />
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/login" className="nav-link">
+              login
               </Link>
-            </li>
-          </ul>
-        </nav>
-      )
-    } else {
-      return (
-        <nav className="navbar">
-          <ul className="nav">
-            <li className="nav-item">
-              <Link to="/" className="nav-link">
-                Home
+          </li>
+          <li className="nav-item">
+            <Link to="/register" className="nav-link">
+              Register
               </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/login" className="nav-link">
-                login
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to="/register" className="nav-link">
-                Register
-              </Link>
-            </li>
-          </ul>
-        </nav>
-      )
-    }
+          </li>
+        </ul>
+      </nav>
+    )
   }
+}
 
 export default Nav;
