bbddbfa74b698e280820fe9c3bb3bc51f5c1dc26
Michael Daniels <mikeytdan@gmail.com>
2018-09-30 20:26:32 -0600

    * Updated an api path comment

--- rent_app/routes/api/workorder.js
+++ rent_app/routes/api/workorder.js
@@ -8,7 +8,7 @@ router
   .post(middleware.paramsCheck(["text", "workOrderID", ["tenantID", "landlordID"]])) // Requires a 'tenantID' or 'landlordID'
   .post(workOrderController.addComment);
 
-// /api/workOrder/addComment
+// /api/workOrder/update
 router
   .route("/update")
   .post(middleware.paramsCheck([["service", "description", "urgent", "read", "completed"]])) // Requires at least one of the following parameters to be valid
