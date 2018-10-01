d47bd926e00a53e9a6a6d9f9cc69047edbf1ac66
Michael Daniels <mikeytdan@gmail.com>
2018-09-30 20:25:28 -0600

    Tweaked the workOrder endpoints to be camelcase to more consistent with other endpoints

--- rent_app/routes/api/workorder.js
+++ rent_app/routes/api/workorder.js
@@ -2,13 +2,13 @@ const router = require("express").Router();
 const workOrderController = require("../../controllers/workOrderController");
 let middleware = require("../../middleware");
 
-// /api/work-order/add-comment
+// /api/workOrder/addComment
 router
   .route("/addComment")
   .post(middleware.paramsCheck(["text", "workOrderID", ["tenantID", "landlordID"]])) // Requires a 'tenantID' or 'landlordID'
   .post(workOrderController.addComment);
 
-// /api/work-order/add-comment
+// /api/workOrder/addComment
 router
   .route("/update")
   .post(middleware.paramsCheck([["service", "description", "urgent", "read", "completed"]])) // Requires at least one of the following parameters to be valid
