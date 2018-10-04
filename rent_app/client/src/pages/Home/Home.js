b454b0354021442d793e787c826101b715a7da60
William Andrew Swensen <williamandrewswensen@Amys-MacBook-Pro.local>
2018-10-03 20:54:36 -0600

    more styling

--- rent_app/client/src/pages/Home/Home.js
+++ rent_app/client/src/pages/Home/Home.js
@@ -28,16 +28,16 @@ class Home extends Component {
           <Col size="sm-12">
             <Container className="intro">
               <Row>
-                <Col size="md-6">
+                <Col size="md-3">
                   <div className="test">
                     <img src="images/RentKeeper-01.png" id="frontImg" className="img-fluid" alt="RentKeeper" />
                   </div>
                 </Col>
-                <Col size="md-6">
+                <Col size="md-9">
                   <h1>Welcome to Rent Keeper!</h1>
-                  <h4>Your property management solution</h4>
+                  <h4>Your One Stop Property Management Solution</h4>
                   <hr />
-                  <p>RentKeeper.net allows property managers and investors to manage their Properties and Tenants all in one convenient application</p>
+                  {/* <p>RentKeeper.net allows property managers and investors to manage their Properties and Tenants all in one convenient application</p> */}
                 </Col>
               </Row>
             </Container>
@@ -62,7 +62,7 @@ class Home extends Component {
                   <Col size="md-4">
                     <Container>
                       <div className="lowerCase">
-                        <p>Access detailed and clear reporting which allows you as a landlord to always have accurate and up to date information about the state of affairs at each of your properties. Thus assisting with making informed, timely and effective management decisions.</p>
+                        <p>Access detailed and clear reporting which allows you as a landlord to always have accurate and up to date information about each of your properties. Assisting with making informed, timely and effective management decisions.</p>
                       </div>
                     </Container>
                   </Col>
@@ -106,8 +106,9 @@ class Home extends Component {
                   <div className="align-center">
                     <h3>Why use Rent Keeper?</h3>
                     <ul className="align-left lowerCase">
-                    <li>Using Rent Keeper for property management your accounting and efficiency will significantly improve. Save time, money and energy which can be used to solve additional problems while at the same time improving the speed and quality of communications with your tenants.</li>
-                    <li>Access detailed reports for managers will provide up-to-date and reliable information about the state of affairs at each property, which will allow making timely and correct management decisions.</li>
+                    <li>Using Rent Keeper for property management your accounting and efficiency will significantly improve. </li>
+                    <li>Save time, money and energy which can be used to solve additional problems while improving the speed and quality of communications with your tenants.</li>
+                    <li>Access detailed reports for property managers with up-to-date and reliable information about each unit or property.</li>
                     <li>Using Rent Keeper as your property management solution will help increase the efficiency and quality of customer service, reduce costs and increase the profit of your organization.</li>
                     </ul>
                   </div>
