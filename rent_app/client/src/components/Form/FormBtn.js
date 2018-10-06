9f4b5fd2294390fe931443cf7d24a16296abcf75
William Andrew Swensen <williamandrewswensen@Amys-MacBook-Pro.local>
2018-10-05 20:55:25 -0600

    styling and clean-up

--- rent_app/client/src/components/Form/FormBtn.js
+++ rent_app/client/src/components/Form/FormBtn.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export const FormBtn = props => (
-  <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-block btn-success ">
+  <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-block btn-primary ">
     {props.children}
   </button>
 );
